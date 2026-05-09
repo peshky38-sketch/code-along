3bf2e8f65b69319b13dfab323201dbae2d78a499
rachelkanyoro <peshky38@gmail.com>
2026-05-09 17:58:23 +0300

    Add prototype method for displaying place details

--- jss/script.js
+++ jss/script.js
@@ -6,3 +6,11 @@ function Place(location, landmarks, season, notes) {
   this.season = season;
   this.notes = notes;
 }
+Place.prototype.fullDetails = function () {
+  return `
+    <h3>${this.location}</h3>
+    <p><strong>Landmarks:</strong> ${this.landmarks}</p>
+    <p><strong>Time of Year:</strong> ${this.season}</p>
+    <p><strong>Notes:</strong> ${this.notes}</p>
+  `;
+};
