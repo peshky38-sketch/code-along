35c6827117f6e740aecb36d372e07afc12615e2f
rachelkanyoro <peshky38@gmail.com>
2026-05-09 17:57:09 +0300

    Create Place constructor for business logic

---
+++ jss/script.js
@@ -0,0 +1,8 @@
+// BUSINESS LOGIC
+
+function Place(location, landmarks, season, notes) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.season = season;
+  this.notes = notes;
+}
